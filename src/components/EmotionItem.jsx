d749df9c85ac0f18c27a42a16cafaa7ab7feb9ed
Haleychoioi <157025597+Haleychoioi@users.noreply.github.com>
2025-08-09 22:12:40 +0900

    update

---
+++ src/components/EmotionItem.jsx
@@ -0,0 +1,18 @@
+import "./EmotionItem.css";
+import { getEmotionImage } from ".././utils/get-emotion-image";
+
+const EmotionItem = ({ emotionId, emotionName, isSelected, onClick }) => {
+  return (
+    <div
+      className={`EmotionItem ${
+        isSelected ? `EmotionItem_on_${emotionId}` : ""
+      }`}
+      onClick={onClick}
+    >
+      <img src={getEmotionImage(emotionId)} className="emotion_img" />
+      <div className="emotion_name">{emotionName}</div>
+    </div>
+  );
+};
+
+export default EmotionItem;
