634ede7423ac2cce4e8c9dd27d64fde7998b4b48
Haleychoioi <157025597+Haleychoioi@users.noreply.github.com>
2025-08-09 22:24:03 +0900

    refactoring/add readmefile

--- src/hooks/usePageTitle.jsx
+++ src/hooks/usePageTitle.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 const usePageTitle = (title) => {
   useEffect(() => {
-    const $title = document.getElementByTagName("title")[0];
+    const $title = document.getElementsByTagName("title")[0];
     $title.innerText = title;
   }, [title]);
 };
